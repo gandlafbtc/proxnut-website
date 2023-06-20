076a6557e68370bbd2b5c5651971915fe9f162b2
gandlaf21 <gandlaf@proton.me>
2023-06-20 13:56:54 +0900

    init

---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import ComingSoon from './lib/ComingSoon.svelte';
+import Counter from './lib/ComingSoon.svelte'
+</script>
+
+<ComingSoon></ComingSoon>
---
+++ src/lib/ComingSoon.svelte
@@ -0,0 +1,4 @@
+<div style="display: flex; flex-direction: column; gap: 5px;">
+    <img style="width: 100px; height: 100px;" src="/proxnut.svg" alt="" />
+    coming soon
+</div>
