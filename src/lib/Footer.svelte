796711076a7d5a3a485775aa50f9d0c4082e426f
gandlaf21 <gandlaf@proton.me>
2023-07-30 08:16:39 +0200

    footer links

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,9 +2,8 @@
 
 <footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
     <div class="grid grid-flow-col gap-4">
-      <a class="link link-hover">About proxnut</a> 
-      <a class="link link-hover">code</a> 
-      <a class="link link-hover">contact</a>
+      <a class="link link-hover" href="https://github.com/gandlafbtc/proxnut">code</a> 
+      <a class="link link-hover" href="https://gandlaf.com">contact</a>
     </div>
     <div class="text-sm">
         <a href="https://gandlaf.com">Project by @gandlaf21</a>
